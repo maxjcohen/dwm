ee560bf1286e86c61283d73f338973787b61fec7
Max Cohen <max.zagouri@pm.me>
2021-02-23 23:21:34 +0100

    Register slack on tag 3

--- config.h
+++ config.h
@@ -32,6 +32,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Slack"  ,  NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
